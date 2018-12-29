fec0ef752d4d0707d4a976caf7950ba5222ab1ef
Lukas <l.seiler@im-en.com>
2018-12-30 00:48:59 +0100

    init

---
+++ Assets/Scripts/ControlTest.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using RabbitControl;
+
+public class ControlTest : MonoBehaviour {
+
+    public float myFloat;
+    public string myString;
+    public int myInt;
+
+    public Vector3 myVector3;
+
+    public int[] myFloatArray;
+
+    public enum testEnum { TEST, ONE, TWO}
+
+    public testEnum myEnum;
+
+
+    void Start () {
+        SetupRabbitControls();
+        myEnum = testEnum.TWO;
+       
+    }
+
+
+    private void SetupRabbitControls()
+    {
+        var paramGroup = Rabbit.Server.CreateGroup("testGroup");
+
+        myVector3
+            .ToValueParameter(nameof(myVector3),paramGroup)
+            .BindParameterTo(() => myVector3, x => myVector3 = x);
+
+        myFloat
+            .ToValueParameter(nameof(myFloat), paramGroup)
+            .BindParameterTo(() => myFloat, x => myFloat = x);
+
+        myString
+            .ToValueParameter(nameof(myString))
+            .BindParameterTo(() => myString, x => myString = x);
+
+        myInt
+            .ToValueParameter(nameof(myInt))
+            .BindParameterTo(() => myInt, x => myInt = x);
+
+        myEnum
+            .ToEnumParameter(nameof(myEnum))
+            .BindParameterTo(() => myEnum.ToString(), x => myEnum = (testEnum)System.Enum.Parse(typeof(testEnum), x));
+
+        myFloatArray
+            .ToArrayParameter(nameof(myFloatArray))
+            .BindParameterTo(() => myFloatArray, x => myFloatArray = x);
+
+        transform
+            .BindTransformToRabbit("cube");
+
+        var renderer = GetComponentInChildren<MeshRenderer>();
+        renderer.material.color
+            .ToValueParameter("testColor")
+            .BindParameterTo(() => renderer.material.color, x => renderer.material.color = x);
+
+   
+    }
+
+}
---
+++ Assets/Scripts/Rabbit.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using RCP;
+using RCP.Parameters;
+using RCP.Transporter;
+using System;
+using BindingsRx.Bindings;
+using BindingsRx.Filters;
+using BindingsRx;
+using RabbitControl.Conversion;
+
+
+namespace RabbitControl
+{
+
+    public class Rabbit : MonoBehaviour
+    {
+
+        public string RemoteHost = "127.0.0.1";
+        public int RemotePort = 4444;
+
+
+        private static RCPServer _server;
+        public static RCPServer Server
+        {
+            get
+            {
+                if (_server == null)
+                {
+                    _server = new RCPServer();
+                }
+                return _server;
+            }
+        }
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+
+        private void Initialize()
+        {
+            Server.AddTransporter(new WebsocketServerTransporter(RemoteHost, RemotePort));     
+            Server.ParameterAdded += (s,p) => { Server.Update(); };
+            Server.ParameterRemoved += (s, p) => { Server.Update(); };
+            Debug.Log("RabbitControl initialized");       
+        }
+        
+    }
+
+    namespace Conversion
+    {
+        public static class Converters
+        {
+            //public static System.Numerics.Vector3 ToSystemVector(this UnityEngine.Vector3 vec)
+            //{
+            //    return new System.Numerics.Vector3(vec.x, vec.y, vec.z);
+            //}
+
+            //public static UnityEngine.Vector3 ToUnityVector(this System.Numerics.Vector3 vec)
+            //{
+            //    return new UnityEngine.Vector3(vec.X, vec.Y, vec.Z);
+            //}
+
+            public static Vector4 ToVector(this UnityEngine.Quaternion vec)
+            {
+                return new Vector4(vec.x, vec.y, vec.z, vec.w);
+            }
+
+            public static UnityEngine.Quaternion ToQuaternion(this Vector4 vec)
+            {
+                return new UnityEngine.Quaternion(vec.x, vec.y, vec.z, vec.w);
+            }
+        }
+    }
+
+
+    public static class Extensions
+    {
+
+        public static ValueParameter<T> ToValueParameter<T>(this T input, string name, GroupParameter groupParameter = null)
+        {
+            var param = Rabbit.Server.CreateValueParameter<T>(name, groupParameter);
+            param.Default = input;
+            param.Value = input;
+            return param;
+        }
+
+      
+
+        public static EnumParameter ToEnumParameter<T>(this T input, string name, GroupParameter groupParameter = null)
+        {
+            var param = Rabbit.Server.CreateEnumParameter(name, groupParameter);
+            param.Entries = System.Enum.GetNames(typeof(T));
+            param.Default = input.ToString();
+            return param;
+        }
+
+        public static ArrayParameter<T> ToArrayParameter<T>(this T[] input, string name, GroupParameter groupParameter = null)
+        {
+            var param = Rabbit.Server.CreateArrayParameter<T>(name, groupParameter);
+            param.Default = input;
+            return param;
+        }
+
+        public static GroupParameter BindTransformToRabbit(this Transform input, string name, GroupParameter groupParameter = null)
+        {
+            var transformGroup = Rabbit.Server.CreateGroup(name + " transform", groupParameter);
+
+            input.BindPositionToRabbit(groupParameter: transformGroup);
+            input.BindRotationToRabbit(groupParameter: transformGroup);
+            input.BindScaleToRabbit(groupParameter: transformGroup);
+
+            return transformGroup;
+        }
+
+        public static ValueParameter<Vector3> BindPositionToRabbit(this Transform input, string name = "Position", GroupParameter groupParameter = null)
+        {         
+            var positionParameter = Rabbit.Server.CreateValueParameter<Vector3>(name);
+            positionParameter.Default = input.position;
+            positionParameter.Value = input.position;
+            input.BindPositionTo(() => positionParameter.Value, x => positionParameter.Value = x);
+
+            return positionParameter;
+        }
+
+        public static ValueParameter<Vector3> BindScaleToRabbit(this Transform input, string name = "Scale", GroupParameter groupParameter = null)
+        {
+            var scaleParameter = Rabbit.Server.CreateValueParameter<Vector3>(name);
+            scaleParameter.Default = input.localScale;
+            scaleParameter.Value = input.localScale;
+            input.BindScaleTo(() => scaleParameter.Value, x => scaleParameter.Value = x);
+
+            return scaleParameter;
+        }
+
+        public static ValueParameter<Vector4> BindRotationToRabbit(this Transform input, string name = "Rotation", GroupParameter groupParameter = null)
+        {
+            var rotationParameter = Rabbit.Server.CreateValueParameter<Vector4>(name);
+            rotationParameter.Default = input.rotation.ToVector();
+            rotationParameter.Value = input.rotation.ToVector();
+            input.BindRotationTo(() => rotationParameter.Value.ToQuaternion(), x => rotationParameter.Value = x.ToVector());
+
+            return rotationParameter;
+        }
+
+        //public static ValueParameter<Color> BindColorToRabbit(this UnityEngine.UI.Image input, string name = "Color", GroupParameter groupParameter = null)
+        //{
+        //    var colorParam = Rabbit.Server.CreateValueParameter<System.Numerics.Vector4>(name);
+        //    rotationParameter.Default = input.rotation.ToSystemVector();
+        //    rotationParameter.Value = input.rotation.ToSystemVector();
+        //    input.bind(() => rotationParameter.Value.ToQuaternion(), x => rotationParameter.Value = x.ToSystemVector());
+
+        //    return rotationParameter;
+        //}
+
+        public static IDisposable BindParameterTo<T>(this ValueParameter<T> input, Func<T> getter, Action<T> setter, BindingTypes bindingType = BindingTypes.Default, params IFilter<T>[] filters)
+        {
+            return GenericBindings.Bind(() => input.Value, x => input.Value = x, getter, setter, bindingType, filters);
+        }
+
+        public static IDisposable BindParameterTo(this EnumParameter input, Func<string> getter, Action<string> setter, BindingTypes bindingType = BindingTypes.Default, params IFilter<string>[] filters)
+        {
+            return GenericBindings.Bind(() => input.Value, x => input.Value = x, getter, setter, bindingType, filters);
+        }
+
+        public static IDisposable BindParameterTo<T>(this ArrayParameter<T> input, Func<T[]> getter, Action<T[]> setter, BindingTypes bindingType = BindingTypes.Default, params IFilter<T[]>[] filters)
+        {
+            return GenericBindings.Bind(() => input.Value, x => input.Value = x, getter, setter, bindingType, filters);
+        }
+
+        
+
+
+
+    }
+
+    
+}
